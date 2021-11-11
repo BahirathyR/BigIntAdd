e45954f972e29dd2b0a609ddfeb2c2c0c9acacd2
Bahirathy <rbahirathy@gmail.com>
2021-11-11 12:56:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## What is this?
+
+In this module used to add Big or Long Integer.
+
+## Installation
+
+npm i BigIntAdd --save
+
+Then...
+
+```
+import {BigIntAdd} from 'BigIntAdd'
+ 
+ var a="234567899909"
+ var b="678905432235"
+
+ BigIntAdd(a,b)
+
+ ```
+
+  ## Add module to import project package.json
+  {
+      "type":"module"
+  }
+
+  ## options
+  var a="234567899909"
+  var b="678905432235"
+ 
+ ## or
+  var a="234567899909"
+  var b="67890543223567886"
+
+  Long Integer value only.
---
+++ index.js
@@ -0,0 +1,31 @@
+var a="345678903344"
+var b="555555555555"
+if(a.length>=b.length){
+    console.log(BigIntAdd(a,b))
+}
+else
+{
+    BigIntAdd(b,a)
+}
+function BigIntAdd(a,b){
+    var sum=""
+    var carry=0;
+    var diff=b.length-a.length;
+    for(i=a.length-1;i>=0;i--){
+    var temp=(Number(a.charAt(i))%10)+(Number(b.charAt(i+diff)%10)+carry)
+    if(temp>=10){
+        sum=temp%10+sum;
+        carry=Math.floor(temp/10)
+    }
+    else{
+        sum=temp+sum;
+        carry=0
+    }
+    }
+    if(carry){
+        sum=carry+sum
+    }
+    return(sum)
+}
+
+module.exports.BigIntAdd=BigIntAdd
